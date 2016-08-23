1cf880ad595436f29fe765a9aea34338f67e449d
Tyler Jefford <tjefford@me.com>
2016-08-22 23:33:15 -0500

    Google requires API key for geo-lookups now. Updated to allow for this refactor unused code add lat/lon to return data add formatted address to return data update readme.

--- Curiosity.jquery.js
+++ Curiosity.jquery.js
@@ -1,193 +1,121 @@
 /*
 ** Curiosity - Location Detection
-** Version 1.0.5 - February 17, 2015
-** Tyler Jefford (tjefford@me.com)
+** Version 2.1 - August 22, 2016
+** Tyler Jefford (tylerjefford.com)
 */
 
 (function($){
-	
-	$.fn.Curiosity = function (action){
-		
-		//Variables
-		var user_streetNumber,
-				user_street,
-				user_zipCode,
-				user_neighborhood,
-				user_county,
-				user_city,
-				user_state,
-				user_state_short,
-				user_country,
-				user_country_short,
-				user_format_address,
-				location_type,
-				hault = 0, //This is for error counting
-				Rover = this; //Rover refers to the element being being called with Curiosity.
-				
-		if(action === 'locate'){
-		
-			var geocoder;	 
-		
-			//Get the latitude and the longitude;
-			function successFunction(position) {
-			    var lat = position.coords.latitude;
-			    var lng = position.coords.longitude;
-			    codeLatLng(lat, lng)
-			}
-			
-			//If error, log it
-			function errorFunction(){
-				console.log("Geocoder failed access to location");
-				return Rover.html('We were unable to locate you.');
-			}
-			
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-			}
-			
-			//Initialize Location
-			window.onload = function initialize() {
-				geocoder = new google.maps.Geocoder();
-			}
-			
-			//Reverse Lookup
-			function codeLatLng(lat, lng) {
-				var latlng = new google.maps.LatLng(lat, lng);
-				if(!geocoder){ //If the onload fails to initiate, start up a new geocoder instance. 
-					geocoder = new google.maps.Geocoder();
-				}
-				geocoder.geocode({'latLng': latlng}, function(results, status) {
-					//If google retuerns 200
-					if (status == google.maps.GeocoderStatus.OK) {
 
-						//Set location data into variables
-						var ZeroCounter = results[0].address_components.length;
-						var Picker = results[0].address_components;
-						
-						//Loop through one data object to pick out a data type
-						//This is much more accurate than using the data index
-						for(var p = 0; p < ZeroCounter; p++){				
-							
-							//Address Data
-							if( Picker[p].types == 'street_number' ){
-								user_streetNumber = Picker[p].long_name;
-							}
-							
-							//Street Name Data
-							if( Picker[p].types == 'route' ){
-								user_street = Picker[p].long_name;
-							}
-							
-							//Zip Code Data
-							if( Picker[p].types == 'postal_code' ){
-								user_zipCode = Picker[p].long_name;
-							}
-							
-							//Neighborhood Data
-							if( Picker[p].types == 'neighborhood,political' ){
-								user_neighborhood = Picker[p].long_name;
-							}
-							
-							//County
-							if( Picker[p].types == 'administrative_area_level_2,political' ){
-								user_county = Picker[p].long_name;
-							}
-							
-							//City Name Data
-							if( Picker[p].types == 'locality,political' ){
-								user_city = Picker[p].long_name;
-							}
-							
-							//State Data
-							if( Picker[p].types == 'administrative_area_level_1,political' ){
-								user_state = Picker[p].long_name;
-								user_state_short = Picker[p].short_name;
-							}
-							
-							//Country Data
-							if( Picker[p].types == 'country,political' ){
-								user_country = Picker[p].long_name;
-								user_country_short = Picker[p].short_name;
-							}	
-						}//end loop
-						
-						//Get additional information
-						
-						//Formatted Street Address
-						if(results[0].formatted_address){
-							user_format_address = results[0].formatted_address;
+	$.fn.Curiosity = function (){
+
+		// Variables
+		var userLocation = [],
+        geocoder
+				$this = (this);
+
+		// Get the Lat / Lon pass to codeLatLng function
+		function successFunction(position) {
+	    var lat = position.coords.latitude;
+	    var lng = position.coords.longitude;
+	    codeLatLng(lat, lng)
+		}
+
+    // Log Errors to the Console, please.
+		function errorFunction(){
+			console.log('Curiosity had an issue locating you, please refresh');
+		}
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+		}
+
+		// Initialize Location
+		window.onload = function initialize() {
+			geocoder = new google.maps.Geocoder();
+		}
+
+		// Reverse Lookup
+		function codeLatLng(lat, lng) {
+		  var latlng = new google.maps.LatLng(lat, lng);
+      if(!geocoder){ //If the onload fails to initiate, start up a new geocoder instance.
+			  geocoder = new google.maps.Geocoder();
+		  }
+
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+				//If google retuerns 200
+				if (status == google.maps.GeocoderStatus.OK) {
+
+					// Set location data into variables
+					// Picker will hold the standard address data provided by google
+					// There is more data returned, and you can see it by dumping `results`
+				  var Picker = results[0].address_components;
+
+					// Each location has unique attributes, so lets loop through all of them
+					// and pick the data we want to use.
+					for(var p = 0; p < Picker.length; p++){
+
+						// Street Number (approx.) Data
+						if( Picker[p].types == 'street_number' ){
+							userLocation.street_number = Picker[p].long_name;
 						}
-						
-						//Location Type
-						if(results[0].geometry.location_type){
-							location_type = results[0].geometry.location_type;
+
+						// Street Name Data
+						if( Picker[p].types == 'route' ){
+							userLocation.street = Picker[p].long_name;
 						}
-						
-						//Loop through all Zone Ranges
-						if(hault == 0){
-							
-							var snap = new Object();
-							
-							if(user_streetNumber){
-								snap.street_number = user_streetNumber;
-							}
-							
-							if(user_street){
-								snap.street_name = user_street;
-							}
-							
-							if(user_city){
-								snap.city = user_city;
-							}
-							
-							if(user_state){
-								snap.state = {'long': user_state, 'short': user_state_short};
-							}
-							
-							if(user_zipCode){
-								snap.zipcode = user_zipCode;
-							}
-							
-							if(user_neighborhood){
-								snap.neighborhood = user_neighborhood;
-							}
-							
-							if(user_county){
-								snap.county = user_county;
-							}
-							
-							if(user_country){
-								snap.country = {'long': user_country, 'short': user_country_short};
-							}
-							
-							if(user_format_address){
-								snap.full_address = user_format_address;
-							}
-							
-							if(latlng){
-								snap.latlon = {'latitude': latlng.k, 'longitude': latlng.D};
-							}
-							
-							if(location_type){
-								snap.location_type = location_type;
-							}
-							
-							
-							var json = JSON.stringify(snap);
-							
-							Rover.text(json);
-							
+
+						// Neighborhood Data
+						if( Picker[p].types == 'neighborhood,political' ){
+							userLocation.neighborhood = Picker[p].long_name;
 						}
+
+						// County Name Data
+						if( Picker[p].types == 'administrative_area_level_2,political' ){
+							userLocation.county = Picker[p].long_name;
+						}
+
+						// Zip Code Data
+						if( Picker[p].types == 'postal_code' ){
+							userLocation.zip = Picker[p].long_name;
+							userLocation.zone = userLocation.zip.substring(0, 3);
+						}
+
+						// City Name Data
+						if( Picker[p].types == 'locality,political' ){
+							userLocation.city = Picker[p].long_name;
+						}
+
+						// State Data
+						if( Picker[p].types == 'administrative_area_level_1,political' ){
+							userLocation.state = Picker[p].long_name;
+							userLocation.state_short = Picker[p].short_name;
+						}
+
+						// Country Data
+						if( Picker[p].types == 'country,political' ){
+							userLocation.country = Picker[p].short_name;
+						}
+
+						// Formatted Address Data (aprox.)
+						if( results[0].formatted_address != "" ){
+							userLocation.formetted_address = results[0].formatted_address;
+						}
+
+						// Include the LatLng too.
+						userLocation.latitude = lat;
+						userLocation.longitude = lng;
 					}
-					else{
-						//If Google retuerns a status other than 200
-						return Rover.html('We were unable to locate you.');
-					}
-				});
-			}
-			
-		}//End of locate action
-		
+
+          // Do anything you want now. You have the data!!
+					console.log(userLocation);
+
+				}else{
+  				// Something didnt work with google. Try something else
+  				errorFunction();
+				}
+		  });
+		}
+
 	} //End Curiosity
-	
+
 }(jQuery));
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Curiosity
-A jQuery plugin that returns location data from a user.
+## Synopsis
+
+A jQuery plugin to detect a users location, ping google for a reverse lookup and return detailed data about the location. (street, state, zip, etc.)
+
+## Getting Started
+
+Include jQuery, Google Maps js API and Curiosity.jQuery.js in your project.
+
+```html
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<YOUR KEY HERE>" async=""></script>
+<script type="text/javascript" src="Curiosity.jQuery.js"></script>
+```
+Next just call the function in your javascript. In our example, we have it embedded in the index.html file.
+
+```js
+$(‘html’).Curiosity();
+```
+
+## Data Returned
+Data will var based on your location (obviously)
+
+- Street Number
+- Street Name
+- Neighborhood
+- City
+- County
+- State
+- State Short
+- Country
+- Zip
+- Zone (first three digits of zipcode)
+- Formatted address
+- Latitude / Longitude
+
+## Tests
+
+You should have your location services turned on, otherwise chrome will display this message:
+
+```
+Curiosity had an issue locating you, please refresh
+```
+
+Within Chrome you can emulate sensors which will allow you to test different locations based on a Lat/Lon pair, This will allow you to test for many types of locations, not just where you are working.
+
+## License
+
+Use it, Don't complain.
